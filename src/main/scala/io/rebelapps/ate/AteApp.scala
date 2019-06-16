9067ef1e21e8dc5429b3772c550f26be503458c2
lolczak <lukasz.jan.olczak@gmail.com>
2019-06-16 10:56:29 +0200

    refactoring

--- src/main/scala/io/rebelapps/ate/AteApp.scala
+++ src/main/scala/io/rebelapps/ate/AteApp.scala
@@ -2,15 +2,15 @@ package io.rebelapps.ate
 
 import cats.data.{EitherK, State}
 import cats.mtl.implicits._
-import io.rebelapps.ate.interpreter.argument.{ArgTransformer, Argument}
 import io.rebelapps.ate.interpreter.argument.Argument.arg
+import io.rebelapps.ate.interpreter.argument.{ArgTransformer, Argument}
 import io.rebelapps.ate.interpreter.cmd.cat.{CatCmd, CatTransformer}
-import io.rebelapps.ate.interpreter.cmd.dir.{DirCmd, DirTransformer}
 import io.rebelapps.ate.interpreter.cmd.dir.DirCmd.ls
+import io.rebelapps.ate.interpreter.cmd.dir.{DirCmd, DirTransformer}
 import io.rebelapps.ate.interpreter.{Eval, HoneypotInstances, RunTime}
-import io.rebelapps.ate.parsing.{Cmd, CmdParser, ShellExpr, SimpleArgumentAst}
-import io.rebelapps.ate.util.data.Fix
+import io.rebelapps.ate.parsing.CmdParser
 import io.rebelapps.ate.transform.Ytransform
+import io.rebelapps.ate.util.data.Fix
 
 import scala.io.StdIn
 
@@ -24,7 +24,18 @@ object AteApp extends App with HoneypotInstances {
   val catTransformer = new CatTransformer[T]()
   val argTransformer = new ArgTransformer[T]()
 
-  val transform = Ytransform(Seq(dirTransformer.transform(_), catTransformer.transform(_), argTransformer.transform(_)))
+  val transformer = dirTransformer |+| catTransformer |+| argTransformer
+
+  def Y[A,B](f: (A=>B)=>(A=>B)) = {
+    case class W(wf: W=>A=>B) {
+      def apply(w: W) = wf(w)
+    }
+    val g: W=>A=>B = w => f(w(w))(_)
+    g(W(g))
+  }
+
+//  val transform = Ytransform(Seq(transformer.transform(_)))
+  val transform = Y(transformer.transform(_))
 
   val expr: Fix[T] = ls[T](List(arg[T]("dir1"), arg[T]("dir2")))
 
--- src/main/scala/io/rebelapps/ate/transform/Transformer.scala
+++ src/main/scala/io/rebelapps/ate/transform/Transformer.scala
@@ -3,8 +3,15 @@ package io.rebelapps.ate.transform
 import io.rebelapps.ate.util.data.Fix
 import io.rebelapps.ate.util.syntax._
 
-trait Transformer[A, F[_]] {
+trait Transformer[A, F[_]] { self =>
 
   def transform(f: A => Fix[F]): A =|> Fix[F]
+
+  def |+|(that: Transformer[A, F]): Transformer[A, F] =
+    new Transformer[A, F] {
+
+      override def transform(f: A => Fix[F]): =|>[A, Fix[F]] = self.transform(f) orElse that.transform(f)
+
+    }
 
 }
